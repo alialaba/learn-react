2824ac5e4ed34e966341ea4fde0fdc42fcdd141b
alialaba <alialaba079@gmail.com>
2023-06-14 22:46:18 -0400

    added props to component

--- vite-project/src/components/Card.jsx
+++ vite-project/src/components/Card.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import Img from "../assets/picture.png"
-export default function Card(){
+export default function Card({title, tools}){
     return(
         <article className="card">
            <img className="card__img" src={Img} alt="pop up" />
            <div>
-                <h4 className="card__title">Design Portfolio</h4>
+                <h4 className="card__title">{title}</h4>
            <div  className="card__tools">
-                <span>HTML</span>
-                <span>CSS</span>
+                {tools.map(tool => <span key={tool}>{tool}</span>)}
            </div>
            </div>
            </article>
